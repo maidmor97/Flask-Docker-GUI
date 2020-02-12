4b63a960b6bc32564cdf31e712c6473ea001ddfe
Jakob Leibetseder <leibetseder@student.tugraz.at>
2020-02-12 16:19:40 +0100

    Implemented inexact name pattern matching for image api

--- backend/controller.py
+++ backend/controller.py
@@ -1,5 +1,5 @@
 import shell_command_runner
-def images():
+def images(name=None):
     command_output = shell_command_runner.run(['docker', 'images'])
     lno = 1
     images = []
@@ -9,5 +9,9 @@ def images():
             tokens.append(word)
         if len(tokens) != 7:
             continue
-        images.append({'name': tokens[0], 'tag': tokens[1], 'id': tokens[2]})
+        if name is None:
+            images.append({'name': tokens[0], 'tag': tokens[1], 'id': tokens[2]})
+            continue
+        if not tokens[0].partition(name)[1] == "":
+            images.append({'name': tokens[0], 'tag': tokens[1], 'id': tokens[2]})
     return images
